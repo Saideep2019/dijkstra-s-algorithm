738b406d6599a7f3b81350655fbdcbc38ebfe47f
Saideep Ambari <saideepambari@gmail.com>
2024-11-28 13:40:44 -0700

    made changes

--- src/main.h
+++ src/main.h
@@ -1,21 +1,54 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <iostream>
+#include <string>
+#include <stdexcept>
 #include "graph.h"
 
 // Function to validate command-line arguments
-bool isValid(int argc, char* argv[]);
+inline bool isValid(int argc, char* argv[]) {
+    // Check if correct number of arguments is provided
+    if (argc != 4) {
+        return false;
+    }
+
+    // Check if graph type is valid
+    std::string graphType = argv[2];
+    if (graphType != "DirectedGraph" && graphType != "UndirectedGraph") {
+        return false;
+    }
+
+    // Check if flag is a valid integer
+    try {
+        std::stoi(argv[3]);
+    } catch (const std::invalid_argument& e) {
+        return false;
+    }
+
+    return true;
+}
 
 // Function to print usage error when incorrect arguments are provided
-void printUsageError();
+inline void printUsageError() {
+    std::cerr << "Usage: " << "./program <input_file> <graph_type> <flag>" << std::endl;
+    std::cerr << "Graph type must be 'DirectedGraph' or 'UndirectedGraph'" << std::endl;
+    std::cerr << "Flag should be an integer" << std::endl;
+}
 
 // Function to print an invalid instruction message
-void printInvalidInstruction();
+inline void printInvalidInstruction() {
+    std::cout << "Invalid instruction. Valid instructions are:" << std::endl;
+    std::cout << "- Stop" << std::endl;
+    std::cout << "- PrintADJ" << std::endl;
+    std::cout << "- SinglePair <src> <dest>" << std::endl;
+    std::cout << "- SingleSource <src>" << std::endl;
+    std::cout << "- PrintLength <src> <dest>" << std::endl;
+    std::cout << "- PrintPath <src> <dest>" << std::endl;
+}
 
-// Function to print the shortest path between two vertices
+// Function declarations for other functions that are likely defined elsewhere
 void printPath(pGRAPH graph, int src, int dest);
-
-// Main instruction processing function
 void getInstructions(pGRAPH graph);
 
 #endif // MAIN_H
